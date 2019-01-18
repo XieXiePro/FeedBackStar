0b441d823acc40403ef3a9c1590e8286c04e29d8
XiePing <2975837870@qq.com>
2019-01-18 17:24:27 +0800

    添加项目说明文件

--- pages/report/feedback.vue
+++ pages/report/feedback.vue
@@ -32,9 +32,9 @@
             </view>
         </view>
 		<!-- 提交 -->
-<!-- 		<view class="foot">
+		<view class="foot">
 			<button type="default"  @click="send()">反馈</button>
-		 </view> -->
+		 </view>
     </view>
 </template>
 
@@ -85,16 +85,46 @@
                 });
             },
             send() { //发送反馈
-                console.log(JSON.stringify(this.sendDate));
-                let imgs = this.imageList.map((value, index) => {
-                    return {
-                        name: "image" + index,
-                        uri: value
-                    }
-                })											              
-            }
-        }
-    }
+				// require插件名称
+				const dcRichAlert = uni.requireNativePlugin('DCloud-RichAlert');  
+			   // 使用插件
+			   dcRichAlert.show({
+			       position: 'bottom',
+			       title: "提示信息",
+			       titleColor: '#FF0000',
+			       content: "<a href='https://uniapp.dcloud.io/' value='Hello uni-app'>uni-app</a> 是一个使用 Vue.js 开发跨平台应用的前端框架!\n免费的\n免费的\n免费的\n重要的事情说三遍",
+			       contentAlign: 'left',
+			       checkBox: {
+			           title: '不再提示',
+			           isSelected: true
+			       },
+			       buttons: [{
+			           title: '退下'
+			       },
+			       {
+			           title: '明白',
+			           titleColor: '#3F51B5'
+			       }
+			       ]
+			   }, result => {
+			       switch (result.type) {
+			           case 'button':
+			               console.log("callback---button--" + result.index);
+			               break;
+			           case 'checkBox':
+			               console.log("callback---checkBox--" + result.isSelected);
+			               break;
+			           case 'a':
+			               console.log("callback---a--" + JSON.stringify(result));
+			               break;
+			           case 'backCancel':
+			               console.log("callback---backCancel--");
+			               break;
+			      }
+			   });
+			}
+		}
+	}
 </script>
 
 <style>
